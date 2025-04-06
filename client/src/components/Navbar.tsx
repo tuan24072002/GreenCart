d05655d3da737e4f960457e2856bcecac6ecf56e
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2025-04-06 23:06:23 +0700

    update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -108,12 +108,15 @@ const Navbar = () => {
                 </div>
                 {
                     open &&
-                    <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+                    <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-50`}>
                         <NavLink to={"/"} onClick={() => setOpen(false)}>Home</NavLink>
                         <NavLink to={"/products"} onClick={() => setOpen(false)}>Products</NavLink>
                         {
                             user &&
-                            <NavLink to={"/my-orders"} onClick={() => setOpen(false)}>My Orders</NavLink>
+                            <>
+                                <NavLink to={"/my-orders"} onClick={() => setOpen(false)}>My Orders</NavLink>
+                                <NavLink to={"/settings"} onClick={() => setOpen(false)}>Settings</NavLink>
+                            </>
                         }
                         <NavLink to={"/contact"} onClick={() => setOpen(false)}>Contact</NavLink>
                         {
